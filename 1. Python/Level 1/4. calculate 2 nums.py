6a8cf1f3bd9a9dd92edc9352cd067edbd6003b6e
Maria Ramy <mariaaramy.official@gmail.com>
2025-04-04 16:32:45 +0200

    Create 4. calculate 2 nums.py

---
+++ 1. Python/Level 1/4. calculate 2 nums.py
@@ -0,0 +1,8 @@
+#Create a program that asks for two numbers and prints their sum, difference, product and quotient
+num1=float(input("Enter number 1: "))
+num2=float(input("Enter number 2: "))
+sum=num1+num2
+difference =num1 - num2
+product=num1*num2
+quotient=num1/num2
+print(f"sum = {sum},diff = {difference},product = {product},quotient = {quotient}")
