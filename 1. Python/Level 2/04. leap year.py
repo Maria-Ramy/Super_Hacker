8eb8a472a879c38067c5017db573faeee526029e
Maria Ramy <mariaaramy.official@gmail.com>
2025-04-22 13:23:54 +0200

    Update 04. leap year.py

--- 1. Python/Level 2/04. leap year.py
+++ 1. Python/Level 2/04. leap year.py
@@ -1,6 +1,10 @@
 #Check if a given year is a leap year.
 year=int(input("Enter a year: "))
-if(year % 4 ==0):
+if(year % 4 ==0 and year % 100 !=0) or (year%400 ==0) :
     print("LEAP YEAR")
 else:
     print("NOT LEAP :)")
+    
+# year % 4 == 0 → divisible by 4 ✅
+# year % 100 != 0 → not a century year ❌ (unless...)
+# year % 400 == 0 → it’s okay if divisible by 400 ✅
